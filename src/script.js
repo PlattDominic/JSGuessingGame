df14eaf304409292dd1a142cb09d39a641d694ae
PlattDominic <74417360+PlattDominic@users.noreply.github.com>
2022-09-08 13:59:16 -0400

    Added more UI
    can't think of a good commit desc

---
+++ src/script.js
@@ -0,0 +1,16 @@
+/*
+    - Dominic Martinez
+*/
+
+
+// Difficulty Enum
+const DifficultyEnum = {
+    Easy: 7,
+    Normal: 5,
+    Insane: 3
+};
+
+// Main HTML element Variables 
+let guessesLeftDisplay = document.getElementById('guesses-left');
+let currentGuessDisplay = document.getElementById('current-guess');
+let guessHintDisplay = document.getElementById('guess-hint');
