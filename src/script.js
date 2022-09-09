8ff430659b5f62dbcd4886a30868217f6dabcb67
Dominic Martinez <64941386+Villix-Main@users.noreply.github.com>
2022-09-09 06:29:47 -0400

    Kinda finished UI and added some JS

--- src/script.js
+++ src/script.js
@@ -11,6 +11,22 @@ const DifficultyEnum = {
 };
 
 // Main HTML element Variables 
+
+let mainTextContainer = document.getElementById('main-game-text');
 let guessesLeftDisplay = document.getElementById('guesses-left');
 let currentGuessDisplay = document.getElementById('current-guess');
 let guessHintDisplay = document.getElementById('guess-hint');
+
+let difficultyPrompt = document.getElementById('difficulty-prompt');
+let difficultyButtons = document.getElementById('difficulty-buttons');
+
+let guessSubmit = document.getElementById('guess-submit');
+
+
+
+initGame = () => {
+    mainTextContainer.style.display = "none";
+    
+}
+
+initGame();
